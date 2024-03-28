12d65b15e9122410f3a3df1ff647b86cf689c62b
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-03-28 14:40:04 -0300

    Definindo rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { DetalhesComponent } from './detalhes/detalhes.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home page'
+    },
+    {
+        path: 'datails/:id',
+        component: DetalhesComponent,
+        title: 'Home detalhes'
+    }
+];
