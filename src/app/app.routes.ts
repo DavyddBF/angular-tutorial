d36afa6024f39cef920c9b6fac058b744dc32574
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-04-02 08:29:44 -0300

    Ajuste de escrita

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
         title: 'Home page'
     },
     {
-        path: 'datails/:id',
+        path: 'detalhes/:id',
         component: DetalhesComponent,
         title: 'Home detalhes'
     }
