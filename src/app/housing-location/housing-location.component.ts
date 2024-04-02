54aab4dec96b0e06a3db8d289708ab2e74d1a2cf
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-04-02 08:30:26 -0300

    Adicionando RouterModule

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,13 +1,15 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HousingLocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule
   ],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
