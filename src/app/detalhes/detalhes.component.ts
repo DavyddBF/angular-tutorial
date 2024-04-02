ec2837c3b3703224e9764b6976d7477a3b355870
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-04-02 08:45:54 -0300

    Importando arquivos | Obtendo parametros da Url

--- src/app/detalhes/detalhes.component.ts
+++ src/app/detalhes/detalhes.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-detalhes',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './detalhes.component.html',
   styleUrl: './detalhes.component.css'
 })
 export class DetalhesComponent {
+  rotas: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId: string = '';
 
+  constructor() {
+    this.housingLocationId = this.rotas.snapshot.params['id'];
+  }
 }
